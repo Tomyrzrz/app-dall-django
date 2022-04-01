56d0ec4051206d363a2bb6e43a294a5dcb7f9ec6
Timo Ruiz <ruiz.timo.ruiz@gmail.com>
2022-03-31 20:05:30 -0600

    Primer Commit

--- appdell/forms.py
+++ appdell/forms.py
@@ -7,7 +7,7 @@ class ComentariosForm(forms.ModelForm):
     fields = ["nombre", "email", "website", "comentario"]
     #fields = '__all__' 
 
-  #nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
+  #nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input', 'required':'true'}))
   #email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input'}))
   #website = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-input'}))
   #comentario = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
